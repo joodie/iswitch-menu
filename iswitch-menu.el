67c33e8e28537a6518a864e4997e83e2b6019707
Joost Diepenmaat <joost@zeekat.nl>
2008-09-14 15:55:06 +0200

    keymaps are now handled as menu specifiers.

--- iswitch-menu.el
+++ iswitch-menu.el
@@ -18,22 +18,29 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;;; Commentary:
+
 
 ;; This code replaces tmm-prompt with something a lot more convenient.
 ;; Prompted by the horrible navigation menus in the otherwise
 ;; *excellent* emacs-rails library, this code should make using menus
 ;; without a mouse or X easy on the brain and fingers.
+;;
+;; to use: put the following in your init file and make sure this
+;; file is in your load-path:
+;;
+;; (require 'iswitch-menu)
+;; (iswitch-menu-override-tmm-prompt)
+;;
+;; you can then access the console menu by running
+;; M-x tmm-menubar
+;; note that this will work even if you're using a GUI menu bar
+;;
+;; to use with emacs-rails navigation: same as above, and switch on
+;; the Rails Always Use Text Menus option in the rails customization
+;; group.
 
-;; to use with emacs-rails: require this code and switch on the Rails
-;; Always Use Text Menus option in the rails customization group.
-
-;;; Code:
-(require 'tmm)
-(require 'iswitchb)
 
 ;; adapted from iswitchb.el, Kin Cho
-
 (defun iswitch-menu-single-prompt (prompt items)
   (let ((iswitchb-make-buflist-hook
 	 (lambda ()
@@ -41,26 +48,77 @@
     (assoc (iswitchb-read-buffer prompt) items)))
 
 (defun iswitch-menu-nested-prompt (prompt items)
-  (message "%s" items)
   (let ((chosen (iswitch-menu-single-prompt (concat prompt " > ") items)))
-    (if (consp (cdr chosen))
+    (if (and (consp (cdr chosen))
+	     (not (eql 'lambda (cadr chosen))))
 	(iswitch-menu-nested-prompt (concat prompt " > " (car chosen)) (cdr chosen))
 	chosen)))
 
-(defun iswitch-menu-parse-keymap (keymap)
-  (error "Not yet implemented")
-  (if (eql 'keymap (car keymap)) ; single keymap
-      (let ((title)
-	    (list))
-	(dolist (item (cdr keymap))
-	  (if (and (not title)
-		   (stringp item))
-	      (setq title item)
-	    (setq list (cons list (iswitch-menu-parse-keymap item)))))
-	(list title (nreverse list)))
-    keymap))
+(defun iswitch-menu-event (thing)
+  (cond
+   ((symbolp thing)
+    thing)
+   ((and (consp thing)
+	 (eql 'lambda (car thing)))
+    (list thing))
+   (t
+    nil)))
 
-(defvar captured-keymap t)
+;; parse a single keymap key definition
+;; (which can be a keymap itself)
+;; TODO: test for visibility/enabled-ness
+(defun iswitch-menu-parse-keymap-entry (def)
+  (when (consp def)
+    (setq def (if (eql 'menu-item (car def))
+		  (cdr def)
+		def))
+    (let ((title (if (stringp (car def)) 
+		     (car def)
+		   (eval (car def)))))
+      (cond
+       ;; ("--")
+       ((not (cdr def))
+	nil)
+       ;; ("Title" . definition)
+       ((iswitch-menu-event (cdr def))
+	(cons title (cdr def)))
+      ;; ("Title" longer definition)
+       ((and (consp (cdr def))
+	     (not (keymapp (cdr def)))
+	     (iswitch-menu-event (cadr def)))
+	(cons title (cadr def)))
+       ;; ("Title" "Description" . def)
+       ((and (consp (cdr def))
+	     (or (stringp (cadr def))
+		 (and (consp (cadr def))
+		      (null (caadr def))))
+	     (iswitch-menu-event (cddr def)))
+	(cons title (cddr def)))
+       ;; nested keymaps
+       ((and (not (symbolp (cdr def)))
+	     (keymapp (cdr def)))
+	(cons title (iswitch-menu-parse-keymap (cdr def))))
+       ((and (not (symbolp (cadr def)))
+	     (keymapp (cadr def)))
+	(cons title (iswitch-menu-parse-keymap (cadr def))))
+       ;;
+       (t
+	(error "iswitch-menu error: can't handle keymap definition %s" def))))))
+
+(defun iswitch-menu-parse-keymap (keymap)
+  (let ((result))
+    (map-keymap (lambda (key def)
+		  (let ((p (iswitch-menu-parse-keymap-entry def)))
+		    (if p
+			(setq result (cons p result))))) keymap)			
+    (cons (if (consp (cadr keymap))
+	      (cadr (cadr keymap))
+	    (cadr keymap)) result)
+    (nreverse result)))
+
+;; this keeps the last requested keymap sent to
+;; iswitch-menu-prompt. useful for debugging
+(defvar iswitch-menu-captured-keymap t)
 
 (defun iswitch-menu-prompt (menu &rest ignored)
   "A drop-in replacement for tmm-prompt and x-popup-menu using iswitchb semantics
@@ -68,29 +126,23 @@ and technology. Should make using menus from the console / keyboard faster and
 more comfortable."
   (if (or (keymapp menu)
 	  (keymapp (car menu)))
-      (setq captured-keymap menu)
-      (iswitch-menu-stored-tmm-prompt menu)
-;      (cdr (apply #'iswitch-menu-nested-prompt (iswitch-menu-parse-keymap menu)))
+      (progn (setq iswitch-menu-captured-keymap menu)
+	     ;; apparently, tmm-prompt calls events from keymaps
+	     (let ((r (cdr (iswitch-menu-nested-prompt "Menu" (iswitch-menu-parse-keymap menu)))))
+	       (call-interactively r)))
     (cdr (iswitch-menu-nested-prompt (caadr menu) (cdadr menu)))))
 
-(defun iswitch-menu-stored-tmm-prompt nil)
+(defvar old-tmm-prompt-set 0)
+(defun old-tmm-prompt ())
 
-(defun iswitch-menu-toggle ()
+(defun iswitch-menu-override-tmm-prompt ()
   (interactive)
-  (cond (#'iswitch-menu-stored-tmm-prompt 
-	 (fset #'tmm-prompt #'iswich-menu-stored-tmm-prompt)
-	 (fset #'iswitch-menu-stored-tmm-prompt nil))
-	(t
-	 (fset #'iswitch-menu-stored-tmm-prompt #'tmm-prompt)
-	 (fset #'tmm-prompt #'iswitch-menu-prompt))))
+  (require 'tmm)
+  (when (= old-tmm-prompt-set 0)
+    (fset #'old-tmm-prompt #'tmm-prompt)
+    (setq old-tmm-prompt-set 1)
+    (fset #'tmm-prompt #'iswitch-menu-prompt)))
 
-(defun iswitch-menu (&optional s)
-  (interactive)
-  (if s
-    (if (> s 1)
-	(iswitch-menu-on)
-      (iswitch-menu-off))
-    (iswitch-menu-toggle)))
 
 (provide 'iswitch-menu)
 ;;; iswitch-menu.el ends here
